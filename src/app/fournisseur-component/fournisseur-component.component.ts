173964c97e97bba6b148d35e65e814fc9d6658ec
Jihed Larayedh <larajihade@gmail.com>
2023-10-02 19:08:36 +0100

    initial Commit

---
+++ src/app/fournisseur-component/fournisseur-component.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fournisseur-component',
+  templateUrl: './fournisseur-component.component.html',
+  styleUrls: ['./fournisseur-component.component.css']
+})
+export class FournisseurComponentComponent {
+  idFournisseur: number = 105;
+  code: string = 'A104B89';
+  libelle: string = 'MyTeck';
+  hide: boolean = false;
+
+  toggleHide() {
+    this.hide = !this.hide;
+    console.log('Hide value:', this.hide);
+  }
+}
